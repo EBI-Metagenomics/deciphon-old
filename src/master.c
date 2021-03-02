4b691b4570429bd9dd9fbf9972ca3a45136bf023
Danilo Horta <danilo.horta@pm.me>
2021-03-02 13:16:46 +0000

    profile_ring

--- src/master.c
+++ src/master.c
@@ -11,20 +11,63 @@
 
 #define BUFFSIZE 256
 
+struct dcp_result
+{
+    uint32_t                 profid;
+    struct imm_result* const null_result;
+    struct imm_result* const alt_result;
+};
+
+struct profile_ring
+{
+    ck_ring_t ring   CK_CC_CACHELINE;
+    ck_ring_buffer_t buffer[BUFFSIZE];
+};
+
+void profile_ring_init(struct profile_ring* ring);
+void profile_ring_push(struct profile_ring* ring, struct dcp_profile const* prof);
+struct dcp_profile const* profile_ring_pop(struct profile_ring* ring);
+
+void profile_ring_init(struct profile_ring* ring)
+{
+    ck_ring_init(&ring->ring, BUFFSIZE);
+    memset(ring->buffer, 0, IMM_ARRAY_SIZE(ring->buffer));
+}
+
+void profile_ring_push(struct profile_ring* ring, struct dcp_profile const* prof)
+{
+    while (!ck_ring_enqueue_spmc(&ring->ring, ring->buffer, prof))
+        ck_pr_stall();
+}
+
+struct dcp_profile const* profile_ring_pop(struct profile_ring* ring)
+{
+    struct dcp_profile const* prof = NULL;
+    bool ok = ck_ring_dequeue_spmc(&ring->ring, ring->buffer, &prof);
+    if (ok)
+        return prof;
+    ck_pr_stall();
+    return NULL;
+}
+
+/* struct result_ring */
+/* { */
+
+/* }; */
+
 void process(char const* seq_str, struct dcp_profile const* prof);
 
 int dcp_master(char const* db_filepath, char const* seq_str)
 {
     struct dcp_input* input = dcp_input_create(db_filepath);
 
-    ck_ring_t ring_spmc CK_CC_CACHELINE;
-    ck_ring_init(&ring_spmc, BUFFSIZE);
-    ck_ring_buffer_t buffer[BUFFSIZE] = {0};
+    struct profile_ring prof_ring;
+    profile_ring_init(&prof_ring);
 
     atomic_bool    finished = false;
     struct elapsed elapsed = elapsed_init();
     elapsed_start(&elapsed);
-#pragma omp parallel default(none) shared(input, seq_str, ring_spmc, buffer, finished)
+#pragma omp parallel default(none) shared(input, seq_str, prof_ring, finished)
     {
 #pragma omp master
         {
@@ -32,26 +75,21 @@ int dcp_master(char const* db_filepath, char const* seq_str)
                 struct dcp_profile const* prof = dcp_input_read(input);
                 printf("dcp_input_read\n");
 
-                while (!ck_ring_enqueue_spmc(&ring_spmc, buffer, prof))
-                    ck_pr_stall();
+                profile_ring_push(&prof_ring, prof);
             }
             finished = true;
         }
 
-        bool ok = true;
+        struct dcp_profile const* prof = NULL;
         do {
-            struct dcp_profile const* prof = NULL;
+            while (!(prof = profile_ring_pop(&prof_ring)) && !finished);
 
-            while (!(ok = ck_ring_dequeue_spmc(&ring_spmc, buffer, &prof)) && !finished) {
-                ck_pr_stall();
-            }
-
-            if (ok) {
+            if (prof) {
                 process(seq_str, prof);
                 dcp_profile_destroy(prof, true);
                 printf("dcp_profile_destroy\n");
             }
-        } while (ok || !finished);
+        } while (prof || !finished);
     }
     dcp_input_destroy(input);
     elapsed_end(&elapsed);
