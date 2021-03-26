7104cd5b2e51b4943e56f51a0731aeaeb88651a2
Danilo Horta <danilo.horta@pm.me>
2021-03-26 00:36:25 +0000

    server_setup server_teardown

--- test/src/pfam24.c
+++ test/src/pfam24.c
@@ -1,40 +1,50 @@
 #include "cass/cass.h"
 #include "dcp/dcp.h"
 
+static struct dcp_server* server_setup(void);
+static void               server_teardown(struct dcp_server* server);
+
 int main(void)
 {
-    char const*        filepath = "/Users/horta/tmp/pfam24.dcp";
-    struct dcp_server* server = dcp_server_create(filepath);
+    struct dcp_server* server = server_setup();
 
-    dcp_server_start(server);
-
-    bool calc_loglik = true;
-    bool calc_null = true;
-    bool multiple_hits = true;
-    bool hmmer3_compat = true;
+    bool                calc_loglik = true;
+    bool                calc_null = true;
+    bool                multiple_hits = true;
+    bool                hmmer3_compat = true;
     struct dcp_task_cfg cfg = {calc_loglik, calc_null, multiple_hits, hmmer3_compat, false};
     struct dcp_task*    task = dcp_task_create(cfg);
 
     dcp_task_add_seq(task, "ACT");
     dcp_server_add(server, task);
 
-    printf("Ponto 1\n");
-    fflush(stdout);
-    while (!dcp_task_end(task)) {
+    /* while (!dcp_task_end(task)) { */
 
-        struct dcp_results* results = dcp_task_read(task);
-        if (!results)
-            continue;
+    /*     struct dcp_results* results = dcp_task_read(task); */
+    /*     if (!results) */
+    /*         continue; */
 
-        dcp_server_recyle(server, results);
-    }
-    /* dcp_task_destroy(task); */
-    printf("Ponto 2\n");
-    fflush(stdout);
-    return 0;
+    /*     dcp_server_recyle(server, results); */
+    /* } */
+    dcp_task_destroy(task);
 
+    server_teardown(server);
+
+    return cass_status();
+}
+
+static struct dcp_server* server_setup(void)
+{
+    char const*        filepath = "/Users/horta/tmp/pfam24.dcp";
+    struct dcp_server* server = dcp_server_create(filepath);
+
+    dcp_server_start(server);
+    return server;
+}
+
+static void server_teardown(struct dcp_server* server)
+{
     dcp_server_stop(server);
     dcp_server_join(server);
     dcp_server_destroy(server);
-    return cass_status();
 }
