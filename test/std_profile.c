3ea8f1929d3722b63c60e7c380d72669b646b5ac
Danilo Horta <danilo.horta@pm.me>
2021-07-22 22:09:14 +0100

    del std_normal.c

--- test/std_profile.c
+++ test/std_profile.c
@@ -16,24 +16,24 @@ void test_std_profile(void)
     struct imm_example1 *m = &imm_example1;
 
     /* Profile 0 */
-    struct dcp_std_profile *prof0 =
-        dcp_std_profile_new(&m->abc, dcp_meta("NAME0", "ACC0"));
-    imm_hmm_reset_dp(m->null.hmm, imm_super(m->null.n), prof0->dp.null);
-    imm_hmm_reset_dp(m->hmm, imm_super(m->end), prof0->dp.alt);
+    struct dcp_meta mt0 = dcp_meta("NAME0", "ACC0");
+    struct dcp_std_profile *p0 = dcp_std_profile_new(&m->abc, mt0);
+    imm_hmm_reset_dp(m->null.hmm, imm_super(m->null.n), p0->dp.null);
+    imm_hmm_reset_dp(m->hmm, imm_super(m->end), p0->dp.alt);
 
     /* Profile 1 */
     struct imm_mute_state *state = imm_mute_state_new(3, &m->abc);
     struct imm_hmm *hmm = imm_hmm_new(&m->abc);
     EQ(imm_hmm_add_state(hmm, imm_super(state)), IMM_SUCCESS);
     EQ(imm_hmm_set_start(hmm, imm_super(state), imm_log(0.3)), IMM_SUCCESS);
-    struct dcp_std_profile *prof1 =
-        dcp_std_profile_new(&m->abc, dcp_meta("NAME1", "ACC1"));
-    imm_hmm_reset_dp(m->null.hmm, imm_super(state), prof1->dp.null);
-    imm_hmm_reset_dp(m->hmm, imm_super(state), prof1->dp.alt);
+    struct dcp_meta mt1 = dcp_meta("NAME1", "ACC1");
+    struct dcp_std_profile *p1 = dcp_std_profile_new(&m->abc, mt1);
+    imm_hmm_reset_dp(m->null.hmm, imm_super(state), p1->dp.null);
+    imm_hmm_reset_dp(m->hmm, imm_super(state), p1->dp.alt);
     imm_del(hmm);
     imm_del(state);
 
-    dcp_std_profile_del(prof0);
-    dcp_std_profile_del(prof1);
+    dcp_std_profile_del(p0);
+    dcp_std_profile_del(p1);
     imm_example1_deinit();
 }
