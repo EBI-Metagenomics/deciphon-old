2caeb5f4dd3b94d5a3f87e9d7e46c1218ccc75bc
Danilo Horta <danilo.horta@pm.me>
2021-09-24 11:31:11 +0100

    cleaning up not used files

--- src/str.c
+++
@@ -1,6 +0,0 @@
-#include "str.h"
-#include "dcp/dcp.h"
-
-char const* dcp_string_data(struct dcp_string const* string) { return stream_data(&string->stream); }
-
-uint32_t dcp_string_size(struct dcp_string const* string) { return (uint32_t)string->size; }
