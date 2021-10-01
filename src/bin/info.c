822b6d2103322ce3a6cc3bd22eaa06a4e62e311f
Danilo Horta <danilo.horta@pm.me>
2021-10-01 09:35:59 +0100

    fix warning

--- src/bin/info.c
+++ src/bin/info.c
@@ -4,4 +4,4 @@
 char const *argp_program_version = "dcp-info " DCP_VERSION;
 char const *argp_program_bug_address = CLI_BUG_ADDRESS;
 
-int main(int argc, char **argv) { return dcp_cli_info(argc, argv); }
+int main(int argc, char **argv) { return (int)dcp_cli_info(argc, argv); }
--- src/bin/press.c
+++ src/bin/press.c
@@ -4,4 +4,4 @@
 char const *argp_program_version = "dcp-press " DCP_VERSION;
 char const *argp_program_bug_address = CLI_BUG_ADDRESS;
 
-int main(int argc, char **argv) { return dcp_cli_press(argc, argv); }
+int main(int argc, char **argv) { return (int)dcp_cli_press(argc, argv); }
