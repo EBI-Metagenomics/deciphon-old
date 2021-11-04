14418635b455282decaed332ff1f1e4cd6d9bea8
Danilo Horta <danilo.horta@pm.me>
2021-11-04 01:21:58 +0000

    filepath

--- src/sched_db.h
+++ src/sched_db.h
@@ -1,21 +1,20 @@
 #ifndef SCHED_DB_H
 #define SCHED_DB_H
 
-#include "filepath.h"
+#include "path.h"
 #include "xstrlcpy.h"
 #include <stdint.h>
 
 struct sched_db
 {
     uint64_t id;
-    char filepath[FILEPATH_SIZE];
+    char filepath[PATH_SIZE];
 };
 
-static inline void sched_abc_init(struct sched_db *db,
-                                  char filepath[FILEPATH_SIZE])
+static inline void sched_abc_init(struct sched_db *db, char filepath[PATH_SIZE])
 {
     db->id = 0;
-    xstrlcpy(db->filepath, filepath, FILEPATH_SIZE);
+    xstrlcpy(db->filepath, filepath, PATH_SIZE);
 }
 
 #endif
--- src/work.c
+++ src/work.c
@@ -6,7 +6,7 @@
 
 static enum dcp_rc prod_file_open(struct prod_file *file)
 {
-    xstrlcpy(file->path, FILEPATH_TEMP_TEMPLATE, MEMBER_SIZE(*file, path));
+    xstrlcpy(file->path, PATH_TEMP_TEMPLATE, MEMBER_SIZE(*file, path));
     enum dcp_rc rc = xfile_mktemp(file->path);
     if (rc) return rc;
     if (!(file->fd = fopen(file->path, "wb")))
