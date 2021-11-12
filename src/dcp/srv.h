948e207d8ddac512fd540bb567e0e2dc5dccceb1
Danilo Horta <danilo.horta@pm.me>
2021-11-12 13:15:54 +0000

    simplify

--- src/dcp/srv.h
+++ src/dcp/srv.h
@@ -24,23 +24,4 @@ DCP_API enum dcp_rc dcp_srv_next_prod(struct dcp_srv *, int64_t job_id,
                                       int64_t *prod_id);
 DCP_API struct dcp_prod const *dcp_srv_get_prod(struct dcp_srv const *srv);
 
-DCP_API void dcp_srv_prod_seq_id(struct dcp_srv *srv, int64_t prod_id,
-                                 int64_t *seq_id);
-DCP_API void dcp_srv_prod_match_id(struct dcp_srv *srv, int64_t prod_id,
-                                   int64_t *match_id);
-DCP_API void dcp_srv_prod_prof_name(struct dcp_srv *srv, int64_t prod_id,
-                                    char prof_name[DCP_PROF_NAME_SIZE]);
-DCP_API void dcp_srv_prod_abc_name(struct dcp_srv *srv, int64_t prod_id,
-                                   char abc_name[DCP_ABC_NAME_SIZE]);
-DCP_API void dcp_srv_prod_loglik(struct dcp_srv *srv, int64_t prod_id,
-                                 double *loglik);
-DCP_API void dcp_srv_prod_null_loglik(struct dcp_srv *srv, int64_t prod_id,
-                                      double *null_loglik);
-DCP_API void dcp_srv_prod_model(struct dcp_srv *srv, int64_t prod_id,
-                                char model[DCP_MODEL_SIZE]);
-DCP_API void dcp_srv_prod_version(struct dcp_srv *srv, int64_t prod_id,
-                                  char version[DCP_VERSION_SIZE]);
-DCP_API void dcp_srv_prod_match_data(struct dcp_srv *srv, int64_t prod_id,
-                                     char match_data[DCP_MATCH_DATA_SIZE]);
-
 #endif
