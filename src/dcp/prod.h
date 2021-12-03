380535e42026cf19ee68ff66918b1307f5edb298
Danilo Horta <danilo.horta@pm.me>
2021-12-03 13:56:56 +0000

    no need to expose prod struct

--- src/dcp/prod.h
+++ src/dcp/prod.h
@@ -5,26 +5,6 @@
 #include "limits.h"
 #include <stdint.h>
 
-struct dcp_prod
-{
-    int64_t id;
-
-    int64_t job_id;
-    int64_t seq_id;
-    int64_t match_id;
-
-    char prof_name[DCP_PROF_NAME_SIZE];
-    char abc_name[DCP_ABC_NAME_SIZE];
-
-    double loglik;
-    double null_loglik;
-
-    char model[DCP_MODEL_SIZE];
-    char version[DCP_VERSION_SIZE];
-
-    char match_data[DCP_MATCH_DATA_SIZE];
-};
-
 IMM_API double dcp_prod_lrt(struct dcp_prod const *prod);
 
 #endif
