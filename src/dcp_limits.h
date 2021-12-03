c8db942d8b455a6de6855389b698810dee70f87a
Danilo Horta <danilo.horta@pm.me>
2021-12-03 14:57:20 +0000

    avoid standard header limits.h collision

---
+++ src/dcp_limits.h
@@ -0,0 +1,16 @@
+#ifndef LIMITS_H
+#define LIMITS_H
+
+#define DCP_ABC_NAME_SIZE 16
+#define DCP_DB_NAME_SIZE 64
+#define DCP_ERROR_SIZE 256
+#define DCP_MODEL_SIZE 16
+#define DCP_FILENAME_SIZE 128
+#define DCP_PATH_SIZE 4096
+#define DCP_PROF_NAME_SIZE 256
+#define DCP_SEQ_NAME_SIZE 256
+#define DCP_SEQ_SIZE (1024 * 1024)
+#define DCP_STATE_SIZE 5
+#define DCP_VERSION_SIZE 16
+
+#endif
