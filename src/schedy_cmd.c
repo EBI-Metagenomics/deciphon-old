7de72491ddb5ca8bb70900af8f50e9e7ed9fba36
Danilo Horta <danilo.horta@pm.me>
2022-08-22 14:40:07 +0100

    dont return newline here

--- src/schedy_cmd.c
+++ src/schedy_cmd.c
@@ -145,66 +145,66 @@ char const *schedy_cmd_db_up(struct getcmd const *gc)
 char const *schedy_cmd_db_dl(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 
 char const *schedy_cmd_db_get_by_id(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 char const *schedy_cmd_db_get_by_xxh3(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 char const *schedy_cmd_db_get_by_job_id(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 char const *schedy_cmd_db_get_by_filename(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 
 char const *schedy_cmd_job_next_pend(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 char const *schedy_cmd_job_set_state(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 char const *schedy_cmd_job_inc_progress(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 
 char const *schedy_cmd_prods_file_up(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 
 char const *schedy_cmd_scan_next_seq(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 char const *schedy_cmd_scan_num_seqs(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 char const *schedy_cmd_scan_get_by_job_id(struct getcmd const *gc)
 {
     (void)gc;
-    return "\n";
+    return "";
 }
 
 static enum rc download_hmm(char const *filepath, void *data)
