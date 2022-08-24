dea70dde6fca82a9b22a21c67dc93048d5e41ddf
Danilo Horta <danilo.horta@pm.me>
2022-08-24 13:50:05 +0100

    test tools

---
+++ test/tools/dotenv_get.c
@@ -0,0 +1,24 @@
+#include "dotenv/dotenv.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define VARS(X)                                                                \
+    X("DCP_NUM_THREADS")                                                       \
+    X("DCP_API_HOST")                                                          \
+    X("DCP_API_PORT")                                                          \
+    X("DCP_API_PREFIX")                                                        \
+    X("DCP_API_KEY")
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) return 1;
+    if (dotenv_load(argv[1], true)) return 1;
+
+#define X(X)                                                                   \
+    if (!strcmp(argv[2], X)) return puts(getenv(X)), 0;
+    VARS(X)
+#undef X
+
+    return 1;
+}
