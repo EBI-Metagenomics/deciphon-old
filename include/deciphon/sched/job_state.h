3ec4bc72525de32d56851a1e957af4695e07988a
Danilo Horta <danilo.horta@pm.me>
2022-08-26 03:16:42 +0100

    schedy_scan

--- include/deciphon/sched/job_state.h
+++ include/deciphon/sched/job_state.h
@@ -4,6 +4,6 @@
 #include "deciphon/sched/sched.h"
 
 char const *job_state_decode(enum sched_job_state state);
-enum sched_job_state job_state_encode(char const *state);
+bool job_state_encode(char const *str, enum sched_job_state *state);
 
 #endif
--- src/sched/job_state.c
+++ src/sched/job_state.c
@@ -2,27 +2,23 @@
 #include <assert.h>
 #include <string.h>
 
+static char const strings[][5] = {"pend", "run", "done", "fail"};
+
 char const *job_state_decode(enum sched_job_state state)
 {
-    if (state == SCHED_PEND) return "pend";
-    if (state == SCHED_RUN) return "run";
-    if (state == SCHED_DONE) return "done";
-    if (state == SCHED_FAIL) return "fail";
-    assert(0);
-    return "";
+    if (state < SCHED_PEND || state > SCHED_FAIL) return 0;
+    return strings[state];
 }
 
-enum sched_job_state job_state_encode(char const *state)
+bool job_state_encode(char const *str, enum sched_job_state *state)
 {
-    if (strcmp("pend", state) == 0)
-        return SCHED_PEND;
-    else if (strcmp("run", state) == 0)
-        return SCHED_RUN;
-    else if (strcmp("done", state) == 0)
-        return SCHED_DONE;
-    else if (strcmp("fail", state) == 0)
-        return SCHED_FAIL;
-
-    assert(0);
-    return 0;
+    for (int i = 0; i < 4; ++i)
+    {
+        if (!strcmp(strings[i], str))
+        {
+            *state = i;
+            return true;
+        }
+    }
+    return false;
 }
