4ce929aed092ad2bd05d19a219f53667e7d91ab3
Danilo Horta <danilo.horta@pm.me>
2022-09-22 23:50:14 +0100

    remove server folder

--- src/server/hmm.h
+++
@@ -1,8 +0,0 @@
-#ifndef HMM_H
-#define HMM_H
-
-#include <stdint.h>
-
-enum rc hmm_press(int64_t job_id, unsigned num_threads);
-
-#endif
--- src/server/scan.h
+++
@@ -1,8 +0,0 @@
-#ifndef SCAN_H
-#define SCAN_H
-
-#include <stdint.h>
-
-enum rc scan_run(int64_t job_id, unsigned num_threads);
-
-#endif
