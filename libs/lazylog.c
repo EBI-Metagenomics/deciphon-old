2e3a46808876ec115f124052af6f995f816f3443
Danilo Horta <danilo.horta@pm.me>
2022-09-30 09:22:17 +0100

    add lazylog

---
+++ libs/lazylog.c
@@ -0,0 +1,69 @@
+#include "lazylog.h"
+#include <stdarg.h>
+#include <stdatomic.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+static struct
+{
+    FILE *fp;
+    char stamp[sizeof("24:00:00")];
+    enum zlog_lvl lvl;
+} self = {NULL, {0}, ZLOG_NOTSET};
+
+static const char *strings[] = {"NOTSET", "DEBUG", "INFO",
+                                "WARN",   "ERROR", "FATAL"};
+
+static atomic_flag flag = ATOMIC_FLAG_INIT;
+
+static inline void lock(void)
+{
+    while (atomic_flag_test_and_set(&flag))
+        ;
+}
+
+static inline void unlock(void) { atomic_flag_clear(&flag); }
+
+bool zlog_setup(char const *sink, enum zlog_lvl lvl)
+{
+    if (!strcmp(sink, "&1"))
+    {
+        self.fp = fdopen(1, "w");
+    }
+    else if (!strcmp(sink, "&2"))
+    {
+        self.fp = fdopen(2, "w");
+    }
+    else if (!sink)
+    {
+        self.fp = fopen("/dev/null", "w");
+    }
+    else
+        self.fp = fopen(sink, "w");
+
+    self.lvl = lvl;
+    return !!self.fp;
+}
+
+void zlog_print(enum zlog_lvl lvl, char const *func, char const *file, int line,
+                char const *fmt, ...)
+{
+    lock();
+    time_t timer = time(NULL);
+    struct tm *tm_info = localtime(&timer);
+    strftime(self.stamp, sizeof(self.stamp), "%H:%M:%S", tm_info);
+
+    if (lvl >= self.lvl)
+    {
+        va_list ap;
+        fprintf(self.fp, "%s | %-5s | %s:%s:%d - ", self.stamp, strings[lvl],
+                func, file, line);
+        va_start(ap, fmt);
+        vfprintf(self.fp, fmt, ap);
+        fprintf(self.fp, "\n");
+        fflush(self.fp);
+        va_end(ap);
+    }
+    unlock();
+}
---
+++ libs/lazylog.h
@@ -0,0 +1,31 @@
+#ifndef LAZYLOG_H
+#define LAZYLOG_H
+
+#include <stdbool.h>
+
+enum zlog_lvl
+{
+    ZLOG_NOTSET,
+    ZLOG_DEBUG,
+    ZLOG_INFO,
+    ZLOG_WARN,
+    ZLOG_ERROR,
+    ZLOG_FATAL,
+};
+
+#define zlog_debug(...)                                                        \
+    zlog_print(ZLOG_DEBUG, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#define zlog_info(...)                                                         \
+    zlog_print(ZLOG_INFO, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#define zlog_warn(...)                                                         \
+    zlog_print(ZLOG_WARN, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#define zlog_error(...)                                                        \
+    zlog_print(ZLOG_ERROR, __func__, __FILE__, __LINE__, __VA_ARGS__)
+#define zlog_fatal(...)                                                        \
+    zlog_print(ZLOG_FATAL, __func__, __FILE__, __LINE__, __VA_ARGS__)
+
+bool zlog_setup(char const *sink, enum zlog_lvl);
+void zlog_print(enum zlog_lvl, char const *func, char const *file, int line,
+                char const *fmt, ...) __attribute__((format(printf, 5, 6)));
+
+#endif
