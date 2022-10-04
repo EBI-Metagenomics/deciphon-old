0566d51d51d9e4dcd0d86855394b6de63cdf9330
Danilo Horta <danilo.horta@pm.me>
2022-10-04 09:55:59 +0100

    xmem, callbacks for loop stuff

---
+++ src/loop/callbacks.h
@@ -0,0 +1,14 @@
+#ifndef LOOP_CALLBACKS_H
+#define LOOP_CALLBACKS_H
+
+#include <stdbool.h>
+
+typedef void on_close_fn_t(void *arg);
+typedef void on_eof_fn_t(void *arg);
+typedef void on_error_fn_t(void *arg);
+typedef void on_open_fn_t(bool ok, void *arg);
+typedef void on_read_fn_t(char *line, void *arg);
+typedef void on_term_fn_t(void);
+typedef void on_exit_fn_t(void);
+
+#endif
