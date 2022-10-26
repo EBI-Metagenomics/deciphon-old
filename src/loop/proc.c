9e14a0745be0efe49c94b9bed0f8b583ffd88e66
Danilo Horta <danilo.horta@pm.me>
2022-10-26 16:31:21 +0100

    proc module

---
+++ src/loop/proc.c
@@ -0,0 +1,28 @@
+#include "loop/proc.h"
+
+void proc_init(struct proc *proc, enum proc_type type) { proc->type = type; }
+
+void proc_setup(struct proc *p, on_read2_fn_t *onrd, on_eof2_fn_t *oneof,
+                on_error2_fn_t *onerr, on_exit_fn_t *onexi)
+{
+    if (p->type == PROC_PARENT) parent_init(&p->parent, onrd, oneof, onerr);
+    if (p->type == PROC_CHILD) child_init(&p->child, onrd, oneof, onerr, onexi);
+}
+
+void proc_start(struct proc *proc, char *args[])
+{
+    if (proc->type == PROC_PARENT) parent_open(&proc->parent);
+    if (proc->type == PROC_CHILD) child_spawn(&proc->child, args);
+}
+
+void proc_send(struct proc *proc, char const *string)
+{
+    if (proc->type == PROC_PARENT) parent_send(&proc->parent, string);
+    if (proc->type == PROC_CHILD) child_send(&proc->child, string);
+}
+
+void proc_stop(struct proc *proc)
+{
+    if (proc->type == PROC_PARENT) parent_close(&proc->parent);
+    if (proc->type == PROC_CHILD) child_kill(&proc->child);
+}
---
+++ src/loop/proc.h
@@ -0,0 +1,30 @@
+#ifndef LOOP_PROC_H
+#define LOOP_PROC_H
+
+#include "loop/child.h"
+#include "loop/parent.h"
+
+enum proc_type
+{
+    PROC_PARENT,
+    PROC_CHILD
+};
+
+struct proc
+{
+    enum proc_type type;
+    union
+    {
+        struct parent parent;
+        struct child child;
+    };
+};
+
+void proc_init(struct proc *, enum proc_type);
+void proc_setup(struct proc *, on_read2_fn_t *, on_eof2_fn_t *,
+                on_error2_fn_t *, on_exit_fn_t *);
+void proc_start(struct proc *, char *args[]);
+void proc_send(struct proc *, char const *string);
+void proc_stop(struct proc *);
+
+#endif
