6ec73e1fdf4fa431580af711378b7814d09bef70
Danilo Horta <danilo.horta@pm.me>
2022-11-22 10:38:12 +0000

    scanny_static

--- test/scan.c
+++ test/scan.c
@@ -1,12 +1,18 @@
+#include "fs.h"
 #include "hope.h"
 #include "logy.h"
 #include "loop/global.h"
 #include "unused.h"
+#include "work.h"
 
 int main(int argc, char *argv[])
 {
     unused(argc);
-    global_init(argv[0], ZLOG_DEBUG, NULL, NULL);
+    global_init(argv[0], ZLOG_DEBUG);
     COND(!global_run_once());
+
+    work_init();
+    // int rc = work_run(seqs, db, prod, true, false);
+
     return hope_status();
 }
