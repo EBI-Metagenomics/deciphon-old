41e33217483a58495583472e14412ddfb7de0a91
Danilo Horta <danilo.horta@pm.me>
2023-06-01 11:53:59 +0100

    pre-commit update

--- lrt.h
+++ lrt.h
@@ -4,7 +4,6 @@
 float lrt32(float null_loglik, float alt_loglik);
 double lrt64(double null_loglik, double alt_loglik);
 
-#define lrt(null, alt)                                                         \
-  _Generic((null), float : lrt32, double : lrt64)(null, alt)
+#define lrt(null, alt) _Generic((null), float: lrt32, double: lrt64)(null, alt)
 
 #endif
